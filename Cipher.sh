6c11f13a3fca07e15336f9f7944e2337df1178d7
cipher486 <61076873+cipher486@users.noreply.github.com>
2020-02-15 18:14:32 +0530

    Update Cipher.sh

--- Cipher.sh
+++ Cipher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m AAshAN ENTERTAINMENT!!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
+echo -e "\e[1;32m CIPHERCALL \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.CIPHERCALL ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By CIPHERCALL >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By AAshAN'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -59,16 +59,16 @@ clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
 git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
+if [[ -s CIPHERCALL/CIPHERCALL.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  CIPHERCALL >> temp
+rm update.445#4N >> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x CIPHERCALL.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m CIPHERCALL Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -99,29 +99,27 @@ echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
 echo "         [-] Mail At: 0n1cOn3@gmx.ch"
 echo ""
 echo ""
-echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
-echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
-echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;31m This Script is Only For Educational Purposes or To ENJOY.\e[0m"
 echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m           Mail: Massmarigamgroup@gmail.com \e[0m"
 echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/massmarigamgroup \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet CIPHERCALL
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border AAshAN
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
+echo -e "\e[1;32m           Mail: massmarigamgroup@gmail.com \e[0m"
 echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/massmarigamgroup \e[0m"
 echo " "
 exit 0
 else
