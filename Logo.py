fa7a30c829935bffa60220635a0d454c06444676
cipher486 <61076873+cipher486@users.noreply.github.com>
2020-02-20 15:42:54 +0530

    Create Logo.py

---
+++ Logo.py
@@ -0,0 +1,23 @@
+clr()
+    logo="""                                                  
+   CCCCCCC
+  CC          
+  CC   PPPP
+  CC  P    P H    H
+  CC  P    P H    H EEEE 
+  CC  P    P H    H E    R    R
+  CC  PPPPP  HHHHHH EEEE  R  R
+  CC  P      HHHHHH EEEE   R 
+  CC  P      H    H E    R   R 
+  CC  P      H    H EEEE   R
+  CC  P      H    H
+  CC  P
+  CC
+   CCCCCCC
+
+
+
+
+                                      """
+    print(random.choice(colors)+logo+W)
+    print("\n")
